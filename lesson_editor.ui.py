70414e8691cc5c80dbdaa267d37c6b534f71476b
Bail(OnePlus PJF110) <2915289604@qq.com>
2024-07-19 20:00:33 +0800

    添加了新增条目快捷键，增加了删除条目功能 已知问题: 1. 新增条目后，键盘焦点不会自动切换到下一个条目 2. 删除最后一个条目后，会留出一行空白(无伤大雅)

--- lesson_editor.ui.py
+++ lesson_editor.ui.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
-def add():
+def add(*_):
     row = len(word_entry_lst)
     wordent = Entry(f2);wordent.grid(row=row,column=0)
     pronent = Entry(f2);pronent.grid(row=row,column=1)
     tranent = Entry(f2);tranent.grid(row=row,column=2)
-    word_entry_lst.append((wordent,pronent,tranent))
+    delbtn = Button(f2,text='-');delbtn.grid(row=row,column=3)
+    tup = (wordent,pronent,tranent,delbtn)
+    delbtn.config(command=lambda:delete(tup))
+    word_entry_lst.append(tup)
+def delete(tup:tuple):
+    for i in tup:
+        i.grid_forget()
+    word_entry_lst.remove(tup)
 
 root = Tk()
 root.title('课程文件编辑器')
+root.bind('<Return>',add)
 f = Frame(root);f.pack()
 Label(f,text='当前文件：a.blf').grid(row=0,column=0)
 Button(f,text='打开').grid(row=0,column=1)
