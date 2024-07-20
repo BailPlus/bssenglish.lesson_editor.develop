a8c17875f83645e4d2c5f7cb91bb499b661f10bb
Bail(OnePlus PJF110) <2915289604@qq.com>
2024-07-20 10:06:34 +0800

    初步学会滚动条

---
+++ scrollframe.py
@@ -0,0 +1,17 @@
+#滚动框架实现方案研究
+from tkinter import *
+root =Tk()
+c = Canvas(root)
+c.pack(fill=BOTH,expand=True)
+f = LabelFrame(c,bg='#ff0000')
+f.pack()#side=LEFT,fill=BOTH,expand=True)
+c.create_window((0,0),window=f)
+s = Scrollbar(c,orient=VERTICAL,command=c.yview)
+s.pack(side=RIGHT,fill=Y)
+c.config(yscrollcommand=s.set)
+f.bind('<Configure>',lambda _:c.config(scrollregion=c.bbox(ALL)))
+
+for i in range(200):
+    Button(f).pack()
+
+root.mainloop()
