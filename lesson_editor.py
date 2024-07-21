84a63ce55c8be7486eea0b9118e7c01bae146ee1
Bail(OnePlus PJF110) <2915289604@qq.com>
2024-07-21 13:26:49 +0800

    实现了打开文件的逻辑 更改了部分变量名称，使其更加正式且易懂

--- lesson_editor.py
+++ lesson_editor.py
@@ -1,51 +1,62 @@
 from tkinter import *
+from tkinter import filedialog
 import libfile
 
 def add(*_):
+    '''添加单词条目'''
     row = len(word_entry_lst)
-    wordent = Entry(f2);wordent.grid(row=row,column=0)
-    pronent = Entry(f2);pronent.grid(row=row,column=1)
-    tranent = Entry(f2);tranent.grid(row=row,column=2)
-    delbtn = Button(f2,text='-');delbtn.grid(row=row,column=3)
-    c.yview_moveto(1)
+    wordent = Entry(words_frame);wordent.grid(row=row,column=0)
+    pronent = Entry(words_frame);pronent.grid(row=row,column=1)
+    tranent = Entry(words_frame);tranent.grid(row=row,column=2)
+    delbtn = Button(words_frame,text='-');delbtn.grid(row=row,column=3)
+    c.yview_moveto(1)   # 滚动到底部
     tup = (wordent,pronent,tranent,delbtn)
     delbtn.config(command=lambda:delete(tup))
     word_entry_lst.append(tup)
 def delete(tup:tuple):
+    '''删除单词条目
+tup(tuple):包含单词条目所有控件的元组'''
     for i in tup:
         i.grid_forget()
     word_entry_lst.remove(tup)
+def openfile():
+    global filename
+    filename = filedialog.askopenfilename(parent=root,title='打开')
+    lesson = libfile.readfile(filename)
+    filename_label.config(text=f'当前文件：{filename}')
+    
 
-word_entry_lst = []
+word_entry_lst = [] # 所有单词条目
+filename:str = None
 
 #配置界面
 root = Tk()
 root.title('课程文件编辑器')
 root.bind('<Return>',add)
-f0 = Frame(root);f0.pack()   # 界面第一行
-Label(f0,text='当前文件：a.blf').grid(row=0,column=0)
-Button(f0,text='打开').grid(row=0,column=1)
-Button(f0,text='保存').grid(row=0,column=2)
-f1 = Frame(root);f1.pack()  # 课程基本信息
-Label(f1,text='简称').grid(row=1,column=0)
-name_entry = Entry(f1)
+filename_frame = Frame(root);filename_frame.pack()   # 界面第一行
+filename_label = Label(filename_frame,text=f'当前文件：{filename}')
+filename_label.grid(row=0,column=0)
+Button(filename_frame,text='打开',command=openfile).grid(row=0,column=1)
+Button(filename_frame,text='保存').grid(row=0,column=2)
+lessoninfo_frame = Frame(root);lessoninfo_frame.pack()  # 课程基本信息
+Label(lessoninfo_frame,text='简称').grid(row=1,column=0)
+name_entry = Entry(lessoninfo_frame)
 name_entry.grid(row=1,column=1)
-Label(f1,text='全称').grid(row=2,column=0)
-fullname_entry = Entry(f1)
+Label(lessoninfo_frame,text='全称').grid(row=2,column=0)
+fullname_entry = Entry(lessoninfo_frame)
 fullname_entry.grid(row=2,column=1)
-Label(f1,text='作者').grid(row=3,column=0)
-auchor_entry = Entry(f1)
+Label(lessoninfo_frame,text='作者').grid(row=3,column=0)
+auchor_entry = Entry(lessoninfo_frame)
 auchor_entry.grid(row=3,column=1)
-# 中间主体
-c = Canvas(root)
+c = Canvas(root)    # 中间主体
 c.pack(fill=BOTH,expand=True)
-f2 = Frame(c)
-f2.pack()
-c.create_window(0,0,window=f2)
+words_frame = Frame(c)
+words_frame.pack()
+c.create_window(0,0,window=words_frame)
 bar = Scrollbar(c,orient=VERTICAL,command=c.yview)
 bar.pack(side=RIGHT,fill=Y)
 c.config(yscrollcommand=bar.set)
-f2.bind_all('<Configure>',lambda _:c.config(scrollregion=c.bbox(ALL)))
+words_frame.bind_all('<Configure>',lambda _:c.config(scrollregion=c.bbox(ALL)))
 '''鼠标滚轮适配参考文献:
 https://www.codenong.com/17355902'''
 c.bind_all('<Button-4>',lambda _:c.yview_scroll(-1,UNITS))  # linux鼠标上键
